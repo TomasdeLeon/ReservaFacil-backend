ba7132f3521f53ea0db1514e15472841a4d9ef34
tomasde_leon <68969911+TomasdeLeon@users.noreply.github.com>
2025-06-10 13:00:13 -0300

    Delete backend/backend directory

--- backend/backend/src/main/java/com/reservas/backend/repository/CaracteristicaRepository.java
+++
@@ -1,6 +0,0 @@
-package com.reservas.backend.repository;
-
-import com.reservas.backend.model.Caracteristica;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CaracteristicaRepository extends JpaRepository<Caracteristica, Long> {}
